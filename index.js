8a1a5f8bf5667a8eecaa525e2f3fb65db53b3c5c
paula <paulalaorga@gmail.com>
2023-09-22 21:31:19 +1000

    index

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch('http://localhost:3000/users', configObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>ID: ${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
